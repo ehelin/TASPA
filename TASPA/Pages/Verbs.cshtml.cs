2e8d95f08ff0a95347f45b7ea16185ffefafe37e
Eric Helin <erichelinse@gmail.com>
2022-08-22 00:39:21 -0500

    Basic link navigation for verb panel

---
+++ TASPA/Pages/Verbs.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TASPA.Pages
+{
+    public class VerbsModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public VerbsModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}

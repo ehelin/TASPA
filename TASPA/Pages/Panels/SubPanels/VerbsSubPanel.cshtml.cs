1d2839c1b591d0b995b033b5305c9a88dc6fb693
Eric Helin <erichelinse@gmail.com>
2022-09-11 21:03:48 -0500

    more updates

---
+++ TASPA/Pages/Panels/SubPanels/VerbsSubPanel.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using Shared.Interfaces;
+using TASPA.Models;
+
+namespace TASPA.Pages
+{
+    public class VerbsSubPanelModel : BaseModel
+    {
+        public VerbsSubPanelModel(ITaspaService taspaService) : base(taspaService) { }
+
+        public void OnGet()
+        {
+            //this.NavigationLinks = this.taspaService.GetNavigationLinks();
+        }
+    }
+}
---
+++ TASPA/wwwroot/js/ServerCalls.js
@@ -0,0 +1,55 @@
+﻿var ServerCalls = {};
+
+ServerCalls.SetVerbList = function (listName) {
+    try {
+        var path = '/TaspaApi/getVerbList?verbListName=' + listName;
+        return ServerCall.Get(path)
+            .then(
+                function (response) {
+                    var jsonParsed = JSON.parse(response);
+
+                    var verbSubPanelContent = document.getElementById("verbSubPanel");
+                    verbSubPanelContent.classList.remove('collasped');
+                    verbSubPanelContent.classList.add('expanded');
+
+                    var verbSubPanelContent = document.getElementById("verbSubPanelContent");
+                    verbSubPanelContent.append(jsonParsed);
+                });
+    }
+    catch (ex) {
+        throw ex;
+    }
+};
+
+//ServerCalls.SetNavigationLinks = function () {
+//    try {
+//        var path = '/TaspaApi/getNavigationLinks';
+//        return ServerCall.Get(path)
+//            .then(
+//                function (response) {
+//                    var jsonParsed = JSON.parse(response);
+//                    var mobileNavigationLinksContainer = document.getElementById("navMenuExpanded");
+//                    var desktopNavigationLinksContainer = document.getElementById("deskNavigationLinks");
+
+//                    var desktopNavigationLinks = '<br />';
+//                    for (var i = 0; i < jsonParsed.length; i++) {
+//                        var currentLink = jsonParsed[i];
+//                        desktopNavigationLinks += '<a href="' + currentLink.linkAction + '">' + currentLink.linkText + '</a>&nbsp;';
+//                    }
+
+//                    desktopNavigationLinksContainer.innerHTML = desktopNavigationLinks;
+
+//                    var mobileNavigationLinks = '<br />';
+//                    mobileNavigationLinks += '<a href="" onclick="SetNavMenu(0);">close</a>';
+//                    for (var i = 0; i < jsonParsed.length; i++) {
+//                        var currentLink = jsonParsed[i];
+//                        mobileNavigationLinks += '<a href="' + currentLink.linkAction + '">' + currentLink.linkText + '</a>';
+//                    }
+
+//                    mobileNavigationLinksContainer.innerHTML = 'hi mom';
+//                });
+//    }
+//    catch (ex) {
+//        throw ex;
+//    }
+//};

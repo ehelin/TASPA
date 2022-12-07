7621bb9d12aa4b298e66424a5f058e414d03b61a
Eric Helin <erichelinse@gmail.com>
2022-12-06 23:19:35 -0600

    start on search results display

---
+++ TASPA/Pages/Panels/SearchResultsPanel.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Shared.Interfaces;
+using TASPA.Models;
+
+namespace TASPA.Pages
+{
+    public class SearchResultsModel : BaseModel
+    {
+        public SearchResultsModel(ITaspaService taspaService) : base(taspaService) { }
+
+        public void OnGet() {}
+    }
+}
---
+++ TASPA/wwwroot/js/SearchResultsPanel.js
@@ -0,0 +1,12 @@
+﻿function InitializeSearch(searchResults) {
+    verbList = [];
+    verbListIndex = 0;
+
+    var searchResults = 
+    for(var ctr=0; ctr<searchResults.Length; ctr++)
+    {
+
+    }
+    
+    var searchResultsPanel = document.getElementById("searchResultsPanel");
+}

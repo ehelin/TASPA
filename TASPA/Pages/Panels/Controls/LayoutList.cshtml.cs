4ad230bbef66efa31468960a0bd5487ae8a5cde8
Eric Helin <erichelinse@gmail.com>
2022-12-28 11:43:43 -0600

    moved common nav links to control

---
+++ TASPA/Pages/Panels/Controls/LayoutList.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Shared.Dto;
+
+namespace TASPA.Pages
+{
+    public class LayoutListModel
+    {
+        public List<NavigationLink> NavigationLinks;
+        public bool IsMobile;
+
+        public LayoutListModel(List<NavigationLink> navigationLinks, bool isMobile) 
+        { 
+            this.NavigationLinks = navigationLinks;
+            this.IsMobile = isMobile;
+        }
+
+        public void OnGet() {}
+    }
+}

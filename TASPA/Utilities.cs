3cc5aecdb03049f8fb5861633db33e1faeca99c4
Eric Helin <erichelinse@gmail.com>
2022-12-28 10:55:36 -0600

    moving search results targer url to utilities class

--- TASPA/Utilities.cs
+++ TASPA/Utilities.cs
@@ -1,9 +1,9 @@
 ﻿using BLL;
 using DAL;
 using Microsoft.Extensions.DependencyInjection;
+using Shared.Dto;
 using Shared.Interfaces;
 
-// TODO - Move to central location
 namespace TASPA
 {
     public class Utilities
@@ -14,6 +14,33 @@ namespace TASPA
             serviceCollection.AddSingleton<ITaspaService, TaspaService>();
 
             return serviceCollection;
+        }
+
+        public static string GetSearchResultsTargetUrl(SearchTerm searchResult)
+        {
+            var searchResultsTargetUrl = string.Empty;
+            if(searchResult.JsonPath.ToLower().Contains("verb"))
+            {
+                searchResultsTargetUrl = string.Format("/Panels/VerbsPanel?selectedSearchTerm={0}", searchResult.Name);
+            }
+            else
+            {
+                var jsonPath = searchResult.JsonPath;
+                var end = jsonPath.LastIndexOf("\\");
+                if(end > 0)
+                {
+                    var vocabularyListName = jsonPath.Substring(0,end);
+                    var start = vocabularyListName.LastIndexOf("\\");
+                    if(start > 0)
+                    {
+                        start += 1;
+                        vocabularyListName = vocabularyListName.Substring(start,vocabularyListName.Length - start);
+                        searchResultsTargetUrl = string.Format("/Panels/VocabularyPanel?selectedSearchTerm={0}&vocabularyList={1}", searchResult.Name, vocabularyListName);
+                    }
+                }
+            }
+
+            return searchResultsTargetUrl;
         }
     }
 }

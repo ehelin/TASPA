c69af122a3451a53ee730726a39238bf13059354
Eric Helin <erichelinse@gmail.com>
2024-01-13 22:46:01 -0600

    chat gpt sample that allows the chat.py script to run and loop getting queries.  Its a step closer to being api callable from the chat app.

--- Tools/RunScript/Program.cs
+++ Tools/RunScript/Program.cs
@@ -59,37 +59,85 @@
 
 //-------------------------------------------------------------------------
 //Script 7
-var eng = IronPython.Hosting.Python.CreateEngine();
-var scope = eng.CreateScope();
-//eng.Execute(@"def greetings(name): 
-//					return 'Hello ' + name.title() + '!'
-//			", scope);
-// 
-eng.Execute(@"import sys
-sys.path.append(r'c:\users\erich\appdata\local\programs\python\python310\lib\site-packages')
-sys.path.append(r'C:\Users\erich\AppData\Local\Programs\Python\Python310\Lib\site-packages\transformers')
-sys.path.append(r'C:\Users\erich\AppData\Local\Programs\Python\Python310\Lib\site-packages\typing_extensions-4.9.0.dist-info')
-sys.path.append(r'C:\Users\erich\AppData\Local\Programs\Python\Python310\Lib\)
-from transformers import AutoModelForCausalLM, AutoTokenizer
-import torch
+//var eng = IronPython.Hosting.Python.CreateEngine();
+//var scope = eng.CreateScope();
+////eng.Execute(@"def greetings(name): 
+////					return 'Hello ' + name.title() + '!'
+////			", scope);
+//// 
+//eng.Execute(@"import sys
+//sys.path.append(r'c:\users\erich\appdata\local\programs\python\python310\lib\site-packages')
+//sys.path.append(r'C:\Users\erich\AppData\Local\Programs\Python\Python310\Lib\site-packages\transformers')
+//#sys.path.append(r'C:\Users\erich\AppData\Local\Programs\Python\Python310\Lib\site-packages\typing_extensions-4.9.0.dist-info')
+//sys.path.append(r'C:\Users\erich\AppData\Local\Programs\Python\Python310\Lib\typing2.py)
+//from transformers import AutoModelForCausalLM, AutoTokenizer
+//import torch
 
-tokenizer = AutoTokenizer.from_pretrained('microsoft/DialoGPT-large')
-model = AutoModelForCausalLM.from_pretrained('microsoft/DialoGPT-large')
+//tokenizer = AutoTokenizer.from_pretrained('microsoft/DialoGPT-large')
+//model = AutoModelForCausalLM.from_pretrained('microsoft/DialoGPT-large')
 
-# Let's chat for 5 lines
-for step in range(500):
-    # encode the new user input, add the eos_token and return a tensor in Pytorch
-    new_user_input_ids = tokenizer.encode(input('>> User:') + tokenizer.eos_token, return_tensors='pt')
+//# Let's chat for 5 lines
+//for step in range(500):
+//    # encode the new user input, add the eos_token and return a tensor in Pytorch
+//    new_user_input_ids = tokenizer.encode(input('>> User:') + tokenizer.eos_token, return_tensors='pt')
 
-    # append the new user input tokens to the chat history
-    bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if step > 0 else new_user_input_ids
+//    # append the new user input tokens to the chat history
+//    bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if step > 0 else new_user_input_ids
 
-    # generated a response while limiting the total chat history to 1000 tokens, 
-    chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
+//    # generated a response while limiting the total chat history to 1000 tokens, 
+//    chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
 
-    # pretty print last ouput tokens from bot
-    print('DialoGPT: {}'.format(tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)))", scope);
+//    # pretty print last ouput tokens from bot
+//    print('DialoGPT: {}'.format(tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)))", scope);
 
-dynamic greetings = scope.GetVariable("greetings");
-System.Console.WriteLine(greetings("world"));
+//dynamic greetings = scope.GetVariable("greetings");
+//System.Console.WriteLine(greetings("world"));
+using System.Diagnostics;
 
+Main();
+
+void Main()
+{
+	// Replace "python.exe" with the actual path to your Python interpreter
+	string pythonPath = "C:\\Users\\erich\\AppData\\Local\\Programs\\Python\\Python310\\python.exe";
+	string scriptPath = "C:\\EricDocuments\\Personal\\Taspa2\\AiModelRunner\\chat.py";
+
+	using (Process process = new Process())
+	{
+		#region
+
+		process.StartInfo.FileName = pythonPath;
+		process.StartInfo.Arguments = scriptPath;
+		process.StartInfo.UseShellExecute = false;
+		process.StartInfo.RedirectStandardInput = true;
+		process.StartInfo.RedirectStandardOutput = true;
+		process.StartInfo.CreateNoWindow = true;
+
+		#endregion
+
+		process.Start();
+
+		using (StreamWriter sw = process.StandardInput)
+		{
+			using (StreamReader sr = process.StandardOutput)
+			{
+				// Send multiple requests
+				for (int i = 0; i < 5; i++)
+				{
+					// Send input to the Python script
+					//sw.WriteLine($"Request {i}");
+					sw.WriteLine("How do you feel?");
+
+					// Receive and process the response
+					string response = sr.ReadLine();
+					Console.WriteLine($"Response {i}: {response}");
+				}
+
+				// Optionally, you can send a termination signal
+				sw.WriteLine("exit");
+			}
+		}
+
+		process.WaitForExit();
+	}
+}

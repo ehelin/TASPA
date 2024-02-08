d64ae145d291ffa2fdaa5be6e8e7b77d3c2b9bbd
Eric Helin <erichelinse@gmail.com>
2024-02-07 21:48:13 -0600

    able to load Llama-2-7b-chat-hf

--- AiModelRunner/chatLlama2.py
+++ AiModelRunner/chatLlama2.py
@@ -1,41 +1,26 @@
+# Load model directly
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf")
+model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-chat-hf")
+
 # from transformers import AutoTokenizer, AutoModelForCausalLM
 
 # tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-70b-chat-hf")
 # model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-70b-chat-hf")
 
-#==================================================================================================
+test = 1
 
-# # prompt = "Hey, are you conscious? Can you talk to me?"
-# # inputs = tokenizer(prompt, return_tensors="pt")
+for step in range(5):
+	prompt = "Hey, are you conscious? Can you talk to me?" 
 
-# # # Generate
-# # generate_ids = model.generate(inputs.input_ids, max_length=30)
-# # tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
+	inputs = tokenizer(prompt, return_tensors="pt") 
 
-# =============================================================================================
+	# Generate
 
-# from transformers import pipeline
+	generate_ids = model.generate(inputs.input_ids, max_length=30) 
 
-# pipe = pipeline("text-generation", model="meta-llama/Llama-2-70b-chat-hf")
+	result = tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
 
-#===============================================================================================
-import json
-
-# Load the LLaMA model
-with open('llama_2_70b_chat_hf.json', 'r') as f:
-    model = json.load(f)
-
-# Define a function to generate responses
-def generate_response(input_text):
-    input_ids = tokenizer.encode(input_text, return_tensors='pt')
-    outputs = model(input_ids)
-    response = tokenizer.decode(outputs[0], skip_special_tokens=True)
-    return response
-
-# Prompt the user to enter text
-print('Enter some text:')
-user_text = input()
-
-# Generate a response
-response = generate_
-
+	print("Llama2: {}".format(tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]))
+# 	#print("DialoGPT: {}".format(tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)))

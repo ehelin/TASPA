7e0dfefb44155e6600a9309f0d585eddbf364358
Eric Helin <erichelinse@gmail.com>
2024-05-18 23:15:33 -0500

    updates

---
+++ AiTests/AiOrchestrationTests.cs
@@ -0,0 +1,12 @@
+namespace AiTests
+{
+    public class AiOrchestrationTests
+    {
+        //https://dzone.com/articles/building-intelligent-applications-using-ai
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
